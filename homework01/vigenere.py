1c522d16b3466db0f3e1b2c25ce338ff468a392f
YuliaBabina <iuliiare03@gmail.com>
2022-10-11 22:13:18 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,26 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    out = ""
+    upp, down = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', "abcdefghijklmnopqrstuvwxyz"
+    keyword = (keyword * len(keyword))[:len(plaintext)]
+    for i in range(len(plaintext)):
+        if plaintext[i] in down:
+            out += down[(down.find(plaintext[i]) + down.find(keyword[i])) % 26]
+        elif plaintext[i] in upp:
+            out += upp[(upp.find(plaintext[i]) + upp.find(keyword[i])) % 26]
+        else:
+            out += plaintext[i]
+    return out
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    out = ""
+    upp, down = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', "abcdefghijklmnopqrstuvwxyz"
+    keyword = (keyword * len(ciphertext))[:len(ciphertext)]
+    for i in range(len(ciphertext)):
+        if ciphertext[i] in down:
+            out += down[(down.find(ciphertext[i]) - down.find(keyword[i]) + 26) % 26]
+        elif ciphertext[i] in upp:
+            out += upp[(upp.find(ciphertext[i]) - upp.find(keyword[i]) + 26) % 26]
+        else:
+            out += ciphertext[i]
+    return out
